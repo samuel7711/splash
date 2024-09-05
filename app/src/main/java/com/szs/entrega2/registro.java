25632726bce0200f0e50cd2f69acdd64f1c7bbdc
samuel7711 <160052004+samuel7711@users.noreply.github.com>
2024-09-05 16:56:56 -0500

    splahs

--- app/src/main/java/com/szs/entrega2/registro.java
+++ app/src/main/java/com/szs/entrega2/registro.java
@@ -30,8 +30,9 @@ public class registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
 
-        register =(Button) findViewById(R.id.register);
+        registrar =(Button) findViewById(R.id.registrar);
         user =(EditText) findViewById(R.id.user);
         password =(EditText) findViewById(R.id.password);
     }
