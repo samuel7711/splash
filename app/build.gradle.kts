ad0fc75273909a141d48ee3ab3da84bd5a4e4783
samuel7711 <160052004+samuel7711@users.noreply.github.com>
2024-09-08 15:07:25 -0500

    splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
     implementation ("com.airbnb.android:lottie:6.4.0")
+    implementation ("com.mikhaellopez:circularimageview:4.3.1")
 
 }
